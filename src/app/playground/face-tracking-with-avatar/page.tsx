ada79533ecac5978e53314131d76dd51a75c766e
Eric Tsai <eric492718@gmail.com>
2023-07-01 22:28:22 +0800

    Modify face-tracking-with-avatar's code to typescript

--- src/app/playground/face-tracking-with-avatar/page.tsx
+++ src/app/playground/face-tracking-with-avatar/page.tsx
@@ -1,15 +1,15 @@
 'use client';
-import './page.css';
-
-import { useEffect, useState } from 'react';
+import { KeyboardEventHandler, useEffect, useRef, useState } from 'react';
 import {
   FaceLandmarker,
   FaceLandmarkerOptions,
   FilesetResolver,
+  DrawingUtils,
+  Category,
 } from '@mediapipe/tasks-vision';
 import { Color, Euler, Matrix4 } from 'three';
 import { Canvas } from '@react-three/fiber';
-import { useDropzone } from 'react-dropzone';
+// import { useDropzone } from 'react-dropzone';
 import Avatar from './components/Avatar';
 
 let video: HTMLVideoElement;
@@ -28,26 +28,34 @@ const options: FaceLandmarkerOptions = {
 };
 
 function App() {
-  const [blendshapes, setBlendshapes] = useState<any[]>([]);
+  const [blendshapes, setBlendshapes] = useState<Category[]>([]);
   const [rotation, setRotation] = useState<Euler>();
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   // https://models.readyplayer.me/648ef0aef2caada0866fd637.glb
-  // https://models.readyplayer.me/649068aea1051fa7234fdbdf.glb
+  // https://models.readyplayer.me/649068aea1051fa7234fdbdf.glb (男)
+  // https://models.readyplayer.me/649fb6cd0b339f947f7c5e2b.glb (女)
   // https://models.readyplayer.me/6490655e99211a8c97fc395f.glb
   // https://models.readyplayer.me/6490674099211a8c97fc3ee9.glb
   const [url, setUrl] = useState<string>(
     'https://models.readyplayer.me/649068aea1051fa7234fdbdf.glb',
   );
-  const { getRootProps } = useDropzone({
-    onDrop: (files) => {
-      const file = files[0];
-      const reader = new FileReader();
-      reader.onload = () => {
-        setUrl(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    },
-  });
+
+  function validateURL(url: string): boolean {
+    const pattern = /^https:\/\/models\.readyplayer\.me\/.*\.glb$/;
+    return pattern.test(url);
+  }
+
+  // const { getRootProps } = useDropzone({
+  //   onDrop: (files) => {
+  //     const file = files[0];
+  //     const reader = new FileReader();
+  //     reader.onload = () => {
+  //       setUrl(reader.result as string);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   },
+  // });
 
   const setup = async () => {
     const filesetResolver = await FilesetResolver.forVisionTasks(
@@ -61,7 +69,7 @@ function App() {
     video = document.getElementById('video') as HTMLVideoElement;
     navigator.mediaDevices
       .getUserMedia({
-        video: { width: 1280, height: 720 },
+        video: { width: 360, height: 360 },
         audio: false,
       })
       .then(function (stream) {
@@ -70,7 +78,85 @@ function App() {
       });
   };
 
+  async function drawMaskOnWebcam() {
+    const videoWidth = 200;
+    const canvasElement = document.getElementById(
+      'output_canvas',
+    ) as HTMLCanvasElement;
+
+    const canvasCtx = canvasElement.getContext('2d')!; // x!; // 告訴 TS，x 這個變數不會是 null 或 undefined
+
+    const radio = video.videoHeight / video.videoWidth;
+    video.style.width = videoWidth + 'px';
+    video.style.height = videoWidth * radio + 'px';
+    canvasElement.style.width = videoWidth + 'px';
+    canvasElement.style.height = videoWidth * radio + 'px';
+    canvasElement.width = video.videoWidth;
+    canvasElement.height = video.videoHeight;
+
+    // Now let's start detecting the stream.
+    let nowInMs = Date.now();
+    let results = undefined;
+    let lastVideoTime = -1;
+    const drawingUtils = new DrawingUtils(canvasCtx);
+    if (lastVideoTime !== video.currentTime) {
+      lastVideoTime = video.currentTime;
+      results = faceLandmarker.detectForVideo(video, nowInMs);
+    }
+    if (results?.faceLandmarks) {
+      for (const landmarks of results.faceLandmarks) {
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+          { color: '#73be73', lineWidth: 1 },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+          { color: '#4d944d' },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+          { color: '#D3D3D3' },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+          { color: '#4d944d' },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+          { color: '#D3D3D3' },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+          { color: '#73be73' },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_LIPS,
+          { color: '#ffcccb' },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+          { color: '#90ee90' },
+        );
+        drawingUtils.drawConnectors(
+          landmarks,
+          FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+          { color: '#90ee90' },
+        );
+      }
+    }
+  }
+
   const predict = async () => {
+    await drawMaskOnWebcam();
+
     let nowInMs = Date.now();
     if (lastVideoTime !== video.currentTime) {
       lastVideoTime = video.currentTime;
@@ -98,8 +184,16 @@ function App() {
     window.requestAnimationFrame(predict);
   };
 
-  const handleOnChange = (event: any) => {
-    setUrl(`${event.target.value}`);
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (event.key !== 'Enter') return;
+    const target = event.target as HTMLInputElement;
+    if (validateURL(target?.value) && event.key === 'Enter') {
+      console.log(target.value);
+      setUrl(target.value);
+      target.value = '';
+    } else {
+      window.alert('URL is not right');
+    }
   };
 
   useEffect(() => {
@@ -108,18 +202,27 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="mx-auto my-0 flex h-[600px] w-[500px] flex-col items-center justify-center rounded-3xl bg-gradient-to-r from-cyan-500 to-blue-500">
       {/* <div {...getRootProps({ className: 'dropzone' })}>
         <p>Drag & drop RPM avatar GLB file here</p>
       </div> */}
       <input
-        className="url"
+        ref={inputRef}
+        className="mt-4 flex h-8 w-4/5 items-center justify-center rounded-xl bg-stone-100 px-[16px]"
         type="text"
         placeholder="Paste RPM avatar URL"
-        onChange={handleOnChange}
+        onKeyDown={handleKeyDown}
       />
-      <video className="camera-feed" id="video" autoPlay></video>
-      <Canvas style={{ height: 380 }} camera={{ fov: 25 }} shadows>
+      <div className="relative mt-4 h-[80px] w-full">
+        <video
+          id="video"
+          className="absolute right-3 top-0 h-full rounded-3xl"
+          autoPlay
+          playsInline></video>
+        <canvas className="absolute right-3 top-0" id="output_canvas"></canvas>
+      </div>
+
+      <Canvas style={{ height: 500 }} camera={{ fov: 25 }} shadows>
         <ambientLight intensity={0.5} />
         <pointLight
           position={[10, 10, 10]}
