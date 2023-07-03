2aff0a0c6c3f15bea36dc5bcf8c23c7f47dc6fdd
Eric Tsai <eric492718@gmail.com>
2023-07-03 16:23:15 +0800

    feat: completed chatGPT basic feature

--- src/app/tech-intro/(nlp)/chat-gpt/page.tsx
+++ src/app/tech-intro/(nlp)/chat-gpt/page.tsx
@@ -1,27 +1,62 @@
-'use client'; // This is a client component
-import getChatGPTApi from '@/utils/openai-api';
-import {useRef} from 'react';
+'use client';
+// 不是很懂，細讀一下
+import { useState, useRef } from 'react';
 
-export default function Content() {
-  const inputRef = useRef<HTMLInputElement>(null);
+function Page() {
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [response, setResponse] = useState('');
 
-  async function sendMessage() {
+  async function getChatGPTResponse(data: string) {
+    const response = await fetch(`/api/chatGPT`, {
+      method: 'POST',
+      body: JSON.stringify({ input: data }),
+    }); // get all response
+    const body = response.body!;
+    const reader = body.getReader();
+    const decoder = new TextDecoder();
+    let wordBuffer = '';
+
+    async function handleWords(words: string[]) {
+      for (const word of words) {
+        if (word === '') continue;
+
+        wordBuffer += word + ' ';
+        setResponse((prevResponse) => prevResponse + wordBuffer); // Update response state
+        await delay(100); // Delay for 0.1 second
+
+        wordBuffer = '';
+      }
+    }
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+
+      const chunk = decoder.decode(value, { stream: true });
+      const words = chunk.split(' ');
+
+      await handleWords(words);
+    }
+  }
+
+  function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function handleSendClick() {
     if (inputRef.current) {
-      const response = await getChatGPTApi(inputRef.current.value);
-      console.log(response);
+      setResponse(''); // Clear previous response
+      await getChatGPTResponse(inputRef.current.value);
     }
   }
 
   return (
-    <main>
-      <input type="text" ref={inputRef} className="border" />
-      <button
-        onClick={() => {
-          sendMessage();
-        }}
-        className="border">
-        點我！！
-      </button>
-    </main>
+    <div>
+      <textarea className="border" ref={inputRef} />
+      <button onClick={handleSendClick}>Send</button>
+      {response && <div className="response">{response}</div>}
+    </div>
   );
 }
+
+export default Page;
