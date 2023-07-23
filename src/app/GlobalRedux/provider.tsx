41295b7b636f9aae17309cf22814c46eca94ed00
Eric Tsai <eric492718@gmail.com>
2023-07-23 17:05:25 +0800

    fix: remove redux practice file

--- src/app/GlobalRedux/provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { Provider } from 'react-redux';
-import { store } from './store';
-
-export function ReduxProviders({ children }: any) {
-  return <Provider store={store}>{children}</Provider>;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css';
-import { ReduxProviders } from './GlobalRedux/provider';
 import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import SideNavbar from '@/components/SideNavbar';
@@ -22,18 +21,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextTopLoader color="linear-gradient(to right, rgb(153, 246, 228), rgb(217, 249, 157))" />
-        {/* 所有 provider 之後可以整合再一起 */}
-        <ReduxProviders>
-          <NextAuthProvider>
-            <div className="w-full">
-              <Header />
-              <div className="flex">
-                <SideNavbar />
-                <div className="mb-20 xl:ml-60 "> {children}</div>
-              </div>
+        <NextAuthProvider>
+          <div className="w-full">
+            <Header />
+            <div className="flex">
+              <SideNavbar />
+              <div className="mb-20 xl:ml-60 "> {children}</div>
             </div>
-          </NextAuthProvider>
-        </ReduxProviders>
+          </div>
+        </NextAuthProvider>
       </body>
     </html>
   );
